78e01030996f7bedb3c2481808d5a35c1e2f1bd0
Hayao S <main@den4dr.com>
2021-09-11 10:11:40 +0900

    initial commit

---
+++ chapter01/cmake-build-debug/CMakeFiles/GameProgrammingCpp.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/den4dr/workspace/personal-project/GameProgrammingCpp/Game.cpp" "/home/den4dr/workspace/personal-project/GameProgrammingCpp/cmake-build-debug/CMakeFiles/GameProgrammingCpp.dir/Game.cpp.o"
+  "/home/den4dr/workspace/personal-project/GameProgrammingCpp/Main.cpp" "/home/den4dr/workspace/personal-project/GameProgrammingCpp/cmake-build-debug/CMakeFiles/GameProgrammingCpp.dir/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/den4dr/.local/vcpkg/installed/x64-linux/include"
+  "/home/den4dr/.local/vcpkg/installed/x64-linux/include/SDL2"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
